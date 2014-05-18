ad9331cf8136875d5bbe698177ac3c874b68afec
ksoria <ksoria15@smpanthers.org>
2014-05-18 16:48:43 -0700

    Update collatz.js

--- collatz.js
+++ collatz.js
@@ -1,1 +1,19 @@
 console.log("Kevin's Collatz");
+
+function collatz(x) {
+  var seq = 'x';
+  
+  while (x > 1) {
+    if (x % 2 == 0) {
+      x = x / 2
+    }
+    else {
+       x = 3*x + 1
+    }
+     seq.append(x)
+  }  
+  return seq
+}
+
+collatz(56)
+  
